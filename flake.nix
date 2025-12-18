99ca877384de52a61e4a0ccaa41452078b6f4bff
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-12-19 00:09:09 +0700

    initialize

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+          ];
+          shellHook = ''
+            echo "Hello from devShell!"
+            exec zsh
+          '';
+        };
+      };
+    };
+}
